7b991e433fcd4d9b944a40f3e6c39f02e7a20388
Sami Hakani <sami.hakani@gmail.com>
2023-11-15 10:28:17 -0500

    Wrote constructor for spectrum wrapper

--- time-scaling/time-scaling.py
+++ time-scaling/time-scaling.py
@@ -46,3 +46,33 @@ class Spectrum():
     Wrapper for testing time scaling of example26 from quspin documentation.
     https://quspin.github.io/QuSpin/examples/example26.html#example26-label
     '''
+    
+    def __init__(self, on_the_fly: bool, L: int):
+        '''
+        Class constructor for wrapper (all initializations)
+        '''
+        self._on_the_fly = on_the_fly   # toggles between using `hamiltonian` or `quantum_LinearOperator`
+        self._L = L     # chain length
+        
+        # Error catching for Heisenberg chain from example
+        if (self._L//2)%2 != 0:
+            raise ValueError("Example requires modifications for Heisenberg chains with L=4*n+2.")
+        if self._L%2 != 0:
+            raise ValueError("Example requires modifications for Heisenberg chains with odd number of sites.")
+        
+        self._S = "1/2" # on-site spin size
+        self._T = (np.arange(self._L)+1) % self._L # translation transformation on sites [0, ..., L-1]
+        
+        # construct basis
+        self._basis0 = spin_basis_general(self._L, S = self._S, m=0, pauli=False, kblock=(T,0))
+        
+        # construct static list for Heisenberg chain
+        self._Jzz_list = [[1.0,i,(i+1)%L] for i in range(self._L)]
+        self._Jxy_list = [[0.5,i,(i+1)%L] for i in range(self._L)]
+        self._static = [["zz", self._Jzz_list],["+-", self._Jxy_list],["-+", self._Jxy_list]]
+        
+        # construct operator for Hamiltonian in the ground state sector
+        if self._on_the_fly:
+            self._H0 = quantum_LinearOperator(self._static, basis = self._basis0, dtype = np.float64)
+        else:
+            self._H0 = hamiltonian(self._static, [], basis = self._basis0, dtype = np.float64)
