9c3dea4946cdbb115256cfdc8e95ad8b088d5688
Sami Hakani <sami.hakani@gmail.com>
2023-11-16 16:26:15 -0500

    Timing only ground state calc

--- time-scaling/spectrum-timing.py
+++ time-scaling/spectrum-timing.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     
     # Initialize dataframe 
     df = pd.DataFrame(columns=['L', 'gs_time', 'sf_time'])
-    sizes = np.array([8 + 2*n for n in range(10) if (8 + 2*n - 2)%4 != 0])
+    sizes = np.array([8 + 2*n for n in range(5) if (8 + 2*n - 2)%4 != 0])
     gs_times = np.zeros(sizes.shape)
     sf_times = np.zeros(sizes.shape)
     
@@ -18,10 +18,12 @@ if __name__ == "__main__":
         mySpectrum.compute_ground_state()
         gs_time = time.time() 
         
-        mySpectrum.compute_spectral_function()
-        sf_time = time.time()
+        # mySpectrum.compute_spectral_function()
+        # sf_time = time.time()
         
-        gs_times[i], sf_times[i] = gs_time - start, sf_time - start 
+        # gs_times[i], sf_times[i] = gs_time - start, sf_time - start 
+        gs_times[i] = gs_time - start 
         
     df.L, df.gs_time, df.sf_time = sizes, gs_times, sf_times 
+    df.L, df.gs_time = sizes, gs_times
     df.to_csv('spectrum-times.csv')
