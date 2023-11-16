726b376136c29853da79eb97b3a13c8b02e18048
Sami Hakani <sami.hakani@gmail.com>
2023-11-16 16:29:11 -0500

    Print system size on each iteration

--- time-scaling/spectrum-timing.py
+++ time-scaling/spectrum-timing.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     sf_times = np.zeros(sizes.shape)
     
     for i, L in enumerate(sizes):
+        print(f'Computing L = {L}')
         mySpectrum = Spectrum(on_the_fly=False, L=int(L), use_symm=False)
         start = time.time()
         
